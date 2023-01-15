c829822abf1afeb11a77dd4326be902924c80f1e
fir <korolyov-work@mail.ru>
2023-01-15 22:48:48 +0300

    Lesson 54

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { followAC, setUsersAC, unfollowAC } from '../../redux/usersReducer';
-import Users from './UsersC';
+import Users from './Users';
 
 let mapStateToProps = (state) => {
     return {

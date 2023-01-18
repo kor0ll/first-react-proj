9132079ebef68feecc98f7eb21af094733a2063c
fir <korolyov-work@mail.ru>
2023-01-18 04:42:50 +0300

    Lesson 56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,68 +1,49 @@
-import axios from 'axios';
 import React from 'react';
 import s from './Users.module.css';
 import userPic from '../../assets/images/user.jpg';
 
-class Users extends React.Component {
 
-    componentDidMount = () => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-        .then(response => {
-            this.props.setUsers(response.data.items);
-            //строка ниже позволяет рассчитывать количество страниц исходя из того, сколько пользователей пришло с сервера
-            //так как их там слишком много, строка будет закомментирована, хардкодом подставлено значение 10, если что меняй в usersReducer
-            //this.props.setTotalUsersCount(response.data.totalCount);
-        })
+let Users = (props) => {
+
+    let pagesCount = Math.ceil( props.totalUsersCount / props.pageSize );
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (page) => {
-        this.props.setCurrentPage(page);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
-        .then(response => {
-            this.props.setUsers(response.data.items);
-        })
-    }
-
-    render = () => {
-
-        let pagesCount = Math.ceil( this.props.totalUsersCount / this.props.pageSize );
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return <div>
-            <div className={s.pageDiv}>
-                {
-                    pages.map(p =>
-                        <span className={p===this.props.currentPage && s.selectedItem}
-                        onClick={(e) => this.onPageChanged(p)}>{p} </span>
-                    )
-                }
-            </div>
+    return <div>
+        <div className={s.pageDiv}>
             {
-                this.props.users.map(u =>
-                    <div key={u.id} className={s.wrapper}>
-                        <span className={s.pic_wrapper}>
-                            <img src={u.photos.small === null ? userPic : u.photos.small} alt="pic" className={s.userPhoto} />
-                            <div>
-                                {u.isfollowed
-                                    ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                            </div>
-                        </span>
-                        <span className={s.textwrapper}>
-                            <div>
-                                <div >{u.name}</div>
-                                <div>{u.status}</div>
-                            </div>
-                            <div>'u.location.city', "u.location.country"</div>
-                        </span>
-                    </div>
+                 pages.map(p =>
+                    {
+                        return <span className={p === props.currentPage && s.selectedItem} onClick={(e) => props.onPageChanged(p)}>{p} </span>
+                    }
                 )
             }
         </div>
-    }
+        {
+            props.users.map(u =>
+                <div key={u.id} className={s.wrapper}>
+                    <span className={s.pic_wrapper}>
+                        <img src={u.photos.small === null ? userPic : u.photos.small} alt="pic" className={s.userPhoto} />
+                        <div>
+                            {u.isfollowed
+                                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                        </div>
+                    </span>
+                    <span className={s.textwrapper}>
+                        <div>
+                            <div >{u.name}</div>
+                            <div>{u.status}</div>
+                        </div>
+                        <div>'u.location.city', "u.location.country"</div>
+                    </span>
+                </div>
+            )
+        }
+    </div>
 }
+
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,6 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { followAC, selectPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from '../../redux/usersReducer';
 import Users from './Users';
+import axios from 'axios';
+
+
+class UsersAPIComponent extends React.Component {
+
+    componentDidMount = () => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+            //строка ниже позволяет рассчитывать количество страниц исходя из того, сколько пользователей пришло с сервера
+            //так как их там слишком много, строка будет закомментирована, хардкодом подставлено значение 10, если что меняй в usersReducer
+            //this.props.setTotalUsersCount(response.data.totalCount);
+        })
+    }
+
+    onPageChanged = (page) => {
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+    render = () => {
+        return <Users totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage} onPageChanged={this.onPageChanged} follow={this.props.follow} unfollow={this.props.unfollow}
+        users={this.props.users}/>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -33,4 +62,4 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
